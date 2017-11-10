e448bfa308897c949f3e00f8e7328fa6af802ae9
LeonelAV <meirinho_leonel@hotmail.com>
2017-11-10 20:05:53 +0100

    defined the inicial state of the App component

--- liftingstateup/src/components/App.js
+++ liftingstateup/src/components/App.js
@@ -3,6 +3,10 @@ import Buttons from './Buttons';
 import Details from './Details.js';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {activeArray:[0,0,0,0], details:""}
+  }
   render(){
     return (
       <div>
