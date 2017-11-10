f0ea95435640308e1b33a1dd7669f3b15e6f5404
LeonelAV <meirinho_leonel@hotmail.com>
2017-11-10 20:34:18 +0100

    Give action to buttons - style added if its active

--- liftingstateup/src/components/Buttons.js
+++ liftingstateup/src/components/Buttons.js
@@ -4,7 +4,7 @@ import React from 'react';
 class Buttons extends React.Component {
   render(){
     return (
-      <button>
+      <button style = {{color: this.props.active? 'red': 'blue'}} onClick = {() =>this.props.clickHandler(this.props.id, this.props.name)}
         {this.props.name}
       </button>
     )
