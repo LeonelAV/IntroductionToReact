68be3f35d0d23c6f2ac4dcc75e5f7386570d61aa
LeonelAV <meirinho_leonel@hotmail.com>
2017-11-14 17:09:32 +0100

    reset button added

--- eventhandlers-counter/src/App.js
+++ eventhandlers-counter/src/App.js
@@ -13,16 +13,22 @@ class Counter extends React.Component {
     super(props)
     this.state = {counter: 0}
     this.clickHandler =  this.clickHandler.bind(this)
+    this.resetHandler = this.resetHandler.bind(this)
   }
 
   clickHandler(){
     this.setState({counter: this.state.counter +1})
   }
 
+  resetHandler(){
+    this.setState({counter: 0})
+  }
+
   render(){
     return (
       <div>
         <button style={buttonStyle} onClick = {this.clickHandler} >{this.state.counter}</button>
+        <button style={{backgroundColor:'red', position:'absolute', right:'53%', top:'15%'}} onClick={this.resetHandler}>RESET</button>
       </div>
     )
   }
