112fe0fe76cc5cd578daf6c67c25b44aa107e83a
LeonelAV <meirinho_leonel@hotmail.com>
2017-11-19 23:44:23 +0100

    added array of answers

--- module2-trivia-app/src/components/TriviaApp.js
+++ module2-trivia-app/src/components/TriviaApp.js
@@ -5,18 +5,28 @@ import Score from '../components/Score';
 
 var questions = [
   ['The Mexican city of Tijuana borders what US city?',3],
-  ['What was the name of the currency used in Spain before the euro?','Franc','Peso','Pesetas','Dollar',2],
-  ['In 1783, the first free flight of a hot air balloon carrying a human occurred in what city?','Berlin','Paris','London','Vienna',1],
+  ['What was the name of the currency used in Spain before the euro?',2],
+  ['In 1783, the first free flight of a hot air balloon carrying a human occurred in what city?',1],
   ['The use of chopsticks originated in what country?','Vietnam','China','Japan','Mongolia',1],
-  ['Napoleon suffered defeat at Waterloo in what year?','1896','1835','1815','1791',2],
-  ['What is the most popular board game of all time?','Chess','Domino','Checkers','Monopoly',0],
-  ['The Roman numeral “D” stands for what number?','200','500','100','1000',1],
-  ['In what year was the first Apple computer released?','1976','1982','1986','1974',0],
-  ['What year was Facebook founded?','2001','2004','2006','1998',1],
-  ['The first McDonald’s restaurant opened in which US state?','Pennsylvania','New York','California','Kentucky',2]
+  ['Napoleon suffered defeat at Waterloo in what year?',2],
+  ['What is the most popular board game of all time?',0],
+  ['The Roman numeral “D” stands for what number?',1],
+  ['In what year was the first Apple computer released?',0],
+  ['What year was Facebook founded?',1],
+  ['The first McDonald’s restaurant opened in which US state?',2]
 ];
 
-var answers = ['Yuma','Calexico','Tucson','San Diego']
+var answers = [
+  ['Yuma','Calexico','Tucson','San Diego'], 
+  ['Franc','Peso','Pesetas','Dollar'], 
+  ['Berlin','Paris','London','Vienna'],
+  ['1896','1835','1815','1791'],
+  ['Chess','Domino','Checkers','Monopoly'],
+  ['200','500','100','1000'],
+  ['1976','1982','1986','1974'],
+  ['2001','2004','2006','1998'],
+  ['Pennsylvania','New York','California','Kentucky']
+  ]
 
 class TriviaApp extends Component {
   constructor(props){
