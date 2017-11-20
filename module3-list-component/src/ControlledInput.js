a3328bdb8bea51407b2e128fb78acc1e2447892d
LeonelAV <meirinho_leonel@hotmail.com>
2017-11-20 18:36:00 +0100

    input element in the view

--- module3-list-component/src/ControlledInput.js
+++ module3-list-component/src/ControlledInput.js
@@ -14,8 +14,8 @@ export class ControlledInput extends React.Component{
   render(){
     return (
       <div>
-      <h1>HELLO</h1>
       <input onChange = {this.handleChange} />
+      <p>{this.state.value}</p>
       </div>
     )
   }
