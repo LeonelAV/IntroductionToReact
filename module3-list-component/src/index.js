319a1fce9223aba2794f667b6b8e96f14650ca00
LeonelAV <meirinho_leonel@hotmail.com>
2017-11-20 17:05:43 +0100

    ProductList component rendering with .map

--- module3-list-component/src/index.js
+++ module3-list-component/src/index.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './ListApp';
+var array = [
+  {id:100, product:"Apple", price:1},
+  {id:101, product:"Banana", price:1},
+  {id:102, product:"Carrot", price:2},
+  {id:103, product:"Donuts", price:5},
+  {id:104, product:"EggPlant", price:4}
+]
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class ProductList extends Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    var elements = this.props.productArray.map( (item, index) => <li key={item.id}>Product: {item.product} | Price: {item.price}</li>)
+    return <ol>{elements}</ol>
+  }
+}
 
+
+ReactDOM.render(<ProductList productArray= {array} />, document.getElementById('root'))
