66fc892b74b6a8d73b94a6f31d9c245ab36c50bf
LeonelAV <meirinho_leonel@hotmail.com>
2017-11-22 17:00:30 +0100

    cretate app component

--- module3-posterapp/src/components/PostList.js
+++ module3-posterapp/src/components/PostList.js
@@ -13,3 +13,4 @@ export function PostList(props) {
     </ol>
   )
 }
+
--- module3-posterapp/src/index.js
+++ module3-posterapp/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {App} from './components/App';
 import {Post} from './components/Post';
 import {PostList} from './components/PostList'
 
-ReactDOM.render(<PostList postList = {[1,2,3,4,5]} />, document.getElementById('root'));
+ReactDOM.render(<App postList = {[1,2,3,4,5]} />, document.getElementById('root'));
 
